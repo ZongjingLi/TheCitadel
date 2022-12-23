5cbdb2a5e24afe6cd749097d631688fcde9bdd53
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-12-23 18:18:28 +0800

    update

---
+++ js_interface.py
@@ -0,0 +1,32 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+import eel
+
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(2,1)
+    def forward(self,x):
+        return self.fc1(x)
+
+net = Net()
+
+@eel.expose              
+def test_func(a, b):
+    return a+b
+
+@eel.expose
+def test_str():
+    x = torch.randn([1,2])
+    x = net(x)
+    res = str(x.detach().numpy()[0][0])
+    return "ACTUAL ANSWER: " + str(res)
+
+@eel.expose
+def run_model():
+    return 0
---
+++ main.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 14 10:03:22 2022
+
+@author: 
+"""
+import eel
+from js_interface import *
+
+
+eel.init('D:\\MecThuen')
+eel.start('core.html') 
