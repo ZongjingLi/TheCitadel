23a51e11cacf8401abbf58f6a4c67ac06c882a0a
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2022-12-24 08:09:06 +0800

    update

---
+++ config.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+import argparse 
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("--name", default = "The Citadel")
+parser.add_argument("--concept_dim",default = 100)
+
+config = parser.parse_args(args = [])
