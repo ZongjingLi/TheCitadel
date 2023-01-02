da5144354b5db95ad5d9a755b5451f1c67d0bb03
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-01-02 15:32:45 +0800

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--name",default = "The Citadel")
+parser.add_argument("--concept_dim",default = 100)
 config = parser.parse_args(args = [])
