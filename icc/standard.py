91c1fa0e0fd14816d3bb643df584c8f85e99283a
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-02 03:05:38 +0800

    add the data structure to the

---
+++ icc/standard.py
@@ -0,0 +1,47 @@
+
+class Stack(object):
+    def __init__():
+        super().__init__()
+
+    def pop():
+        pass
+
+    def add():
+        pass
+
+class Queue(object):
+    def __init__():
+        super().__init__()
+
+    def pop():
+        pass
+
+    def add():
+        pass
+
+
+class BinaryTree(object):
+    def __init__():
+        super().__init__()
+
+    def pop():
+        pass
+
+class Treap(object):
+    def __init__():
+        super().__init__()
+    
+    def add(x):
+        return x
+
+    def pop(k = 1):
+        return k
+
+    def up(self):
+        pass
+
+    def down(self):
+        pass
+
+    def __eq__(self,other):
+        return other == self
