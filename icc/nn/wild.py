3e0f6991b161d3519c75c29f9a38f3b8f307a678
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-06 15:35:22 +0800

    Update wild.py

--- icc/nn/wild.py
+++ icc/nn/wild.py
@@ -2,3 +2,15 @@ import torch
 import torch.nn as nn
 
 import torch.nn.functional as F
+
+class ParticleFilter(nn.Module):
+    def __init__(self, world_size = 50):
+        super().__init__()
+        self.world_size = world_size
+        self.particles = None
+    
+    def forward(self,x):
+        return x
+    
+    def upate_particle(self):
+        return
