f6f0fdc04d4a1634bda9d660d46be48353f38a94
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-06 16:35:33 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     version='0.0',
     description='The Ice Crown Citadel',
     author='Zongjing Li',
-    packages=['icc,citadel'],
+    packages=['icc'],
     author_email='zongjingli997@gmail.com',
 )
