1081d9f65e90cc0b4e194077d79d19d2b1f91023
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-03-07 00:12:35 +0800

    the citadel stands strong

--- test.py
+++ test.py
@@ -20,6 +20,86 @@ ims = read_image("citadel/web/src/arthas.jpg")
 
 from icc.reflection.domains.arithmetics import *
 
-program = Program.parse("(+ 1 1)")
+programs = [
+                ("(+. 1. 1.)",None),
+                ("(*. (+. 1. 1.) pi)",None),
+                ("(+. (*. (+. 1. 1.) pi) 1.)",None),
+                ("(+. (*. (+. 1. 1.) pi) 0.)",None),
+                ("(+. (*. (+. 1. 1.) pi) (*. (+. 1. 1.) pi))",None),
+                ("(+. (*. (+. 1. 1.) pi) (*. (+. 1. 1.) pi))",None),
+            ] 
+
+programs = [
+    ("(+. 1. 1.)",None),
+    ("(+. 1. (+. 0. 1.))",None),
+]
+
+
+
+programs = [(Program.parse(p),t) for p,t in programs ]
+
+arity = 3
+from icc.reflection.version_space import *
+import gc
+
+v = VersionTable(typed=False, identity=False)
+with timing("constructed %d-step version spaces"%arity):
+    versions = [[v.superVersionSpace(v.incorporate(e[0]), arity)] for e in programs]
+    eprint("Enumerated %d distinct version spaces"%len(v.expressions))
+
+    topI = 50
+    candidates = v.bestInventions(versions, bs=3*topI)[:topI]
+
+    eprint("Only considering the top %d candidates"%len(candidates))
+
+    # Clean caches that are no longer needed
+    v.recursiveTable = [None]*len(v)
+    v.inhabitantTable = [None]*len(v)
+    v.functionInhabitantTable = [None]*len(v)
+    v.substitutionTable = {}
+    gc.collect()
+
+CPUs = 1
+with timing("scored the candidate inventions"):
+            scoredCandidates = parallelMap(CPUs,
+                                           lambda candidate: \
+                                           (candidate, .0),
+                                            candidates,
+                                           memorySensitive=True,
+                                           chunksize=1,
+                                           maxtasksperchild=1)
+
+oldScore = 100
+
+if len(scoredCandidates) > 0:
+    bestNew, bestScore = max(scoredCandidates, key=lambda sc: sc[1])
+if len(scoredCandidates) == 0 or bestScore < oldScore:
+            eprint("No improvement possible.")
+            # eprint("Runner-up:")
+            # eprint(next(v.extract(bestNew)))
+            # Return all of the frontiers, which have now been rewritten to use the
+            # new fragments
+
+candidate = bestNew
+new_name = next(v.extract(candidate))
+
+print(new_name,type(new_name))
 
 print(Primitive.GLOBALS)
+
+Primitive(str(new_name), treal, 3.14)
+
+print(Primitive.GLOBALS)
+for bind in Primitive.GLOBALS:
+    funct =  Primitive.GLOBALS[bind]
+    print("function name: {} function type: {}".format(funct,funct.tp))
+
+
+e = Program.parse("(#(lambda (?? (+ 1 $0))) (lambda (?? (+ 1 $0))) (lambda (?? (+ 1 $0))) - * (+ +))")
+print(e)
+
+
+print(programs[0][0].runWithArguments([]))
+
+e = Program.parse("(#(lambda (* (+ 1 $0) 4)))")
+print(e.runWithArguments([3]))
