c31fa65b713f010024cef7cf983f01a8734de2ce
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-06-21 22:28:44 +0800

    update the scene graph and concept learner

--- icc/math/basics.py
+++ icc/math/basics.py
@@ -1,1 +1,13 @@
-from sympy import *
+import torch
+import torch.nn as nn
+
+class Symbol(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+class Expression(nn.Module):
+    def __init__(self):
+        super().__init__()
+    
+    def parse(self, expr):
+        return expr
