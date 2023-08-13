5381e9949c61029563fdaa667d5d2fa6fbbc7369
Melkor <115195620+ZongjingLi@users.noreply.github.com>
2023-08-13 23:11:35 +0800

    Update basics.py

--- icc/math/basics.py
+++ icc/math/basics.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 
+import taichi as ti
+
 class Symbol(nn.Module):
     def __init__(self):
         super().__init__()
@@ -11,3 +13,8 @@ class Expression(nn.Module):
     
     def parse(self, expr):
         return expr
+
+gui = ti.GUI("math_visualize")
+
+while gui.running:
+    gui.show()
